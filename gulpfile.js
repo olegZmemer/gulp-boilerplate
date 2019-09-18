8c4aa3a14ac9807793e454203165a5debb4471d8
olegZmemer <olegZmemer2@gmail.com>
2019-09-18 17:49:09 +0300

    babel deleted, gulp-terser instead gulp-uflify

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,11 @@ const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-terser');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const server = require('browser-sync').create();
-
+const stipcomments = require('gulp-strip-css-comments');
 function html() {
     return src('src/*.html')
         .pipe(htmlmin({
@@ -37,9 +37,10 @@ function styles() {
     return src('src/sass/main.sass')
         .pipe(plumber())
         .pipe(sass())
-        .pipe(postcss([autoprefixer('last 15 version')]))
+        .pipe(postcss([autoprefixer('last 2 version')]))
         .pipe(gcmq())
         .pipe(csso())
+        .pipe(stipcomments())
         .pipe(rename('styles.min.css'))
         .pipe(dest('build/css'))
         .pipe(server.stream());
@@ -48,7 +49,6 @@ function styles() {
 function scripts() {
     return src('src/js/**/*.js')
         .pipe(plumber())
-        .pipe(babel())
         .pipe(uglify())
         .pipe(rename({
             extname: '.min.js'
